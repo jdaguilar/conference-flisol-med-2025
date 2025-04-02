a4b2eef338bcc582adbd6a1a3f3f6cde3c80e304
jdaguilar <jorgeaguilar2761@gmail.com>
2025-04-01 23:59:01 -0500

    Update clear script to remove Nessie and MinIO namespaces

--- clear.sh
+++ clear.sh
@@ -1,6 +1,7 @@
 sudo kubectl delete namespace spark
-sudo kubectl delete namespace trino
+sudo kubectl delete namespace nessie
 sudo kubectl delete namespace dremio
+sudo kubectl delete namespace minio-operator
 
 sudo microk8s kubectl-minio delete -y
 sudo microk8s disable rbac -y
@@ -8,5 +9,4 @@ sudo microk8s disable storage -y
 sudo microk8s disable hostpath-storage -y
 sudo microk8s disable metallb -y
 
-sudo snap remove juju
 sudo snap remove microk8s --purge
